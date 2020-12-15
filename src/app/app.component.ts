9b554c6e9827f63a8121229fd1056928f0fd841d
Your Name <you@example.com>
2020-12-15 16:36:10 -0400

    Projeto Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Pessoa } from './pessoa';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'angular-aulas';
+  nomes = [
+    new Pessoa(1, 'Bonieky', 90),
+    new Pessoa(2, 'Paulo', 20),
+    new Pessoa(3, 'Fulano', 40),
+    new Pessoa(4, 'Cicrano', 30),
+    new Pessoa(5, 'Beltrano', 80),
+    new Pessoa(6, 'João', 5)
+  ];
+  //nomePrincipal = this.nomes[0];
+  nomePrincipal = 'Carlos';
+
+  showAviso = true;
+  avisoMsg = 'Este é um aviso!';
+
+  hideInput = true;
+
+  imagem = 'https://www.google.com.br/google.jpg';
+
+  tamanhoFonte = '25px';
+
+  corPrincipal = '#FF0000';
+
+  trocarCor = function(cor) {
+    this.corPrincipal = cor;
+  }
+
+  clicou = function(pessoa: Pessoa) {
+    alert("Clicou em: "+pessoa.nome+' '+pessoa.getTipo());
+  }
+
+  trocarNome = function(novoNome) {
+    this.nomePrincipal = novoNome;
+  }
 }
